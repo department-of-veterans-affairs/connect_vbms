44a3b3e8b6a58b434633c39d6df41416bb02e9de
Bill Mill <bill.mill@gmail.com>
2014-10-31 11:28:57 -0400

    use the intermediate files folder

--- run.rb
+++ run.rb
@@ -37,8 +37,8 @@ doc.find_first("//saml2:Assertion", ss).remove!
 doc.save("signature_template.xml", :indent => true, :encoding => XML::Encoding::UTF_8)
 
 # sign the doc
-sh "xmlsec1 --sign --privkey-pem cui-tst-client.key --pwd importkey --output signed.xml --id-attr:Id Body --id-attr:Id Timestamp signature_template.xml"
-doc = XML::Parser.file("signed.xml").parse
+sh "xmlsec1 --sign --privkey-pem cui-tst-client.key --pwd importkey --output intermediate_files/signed.xml --id-attr:Id Body --id-attr:Id Timestamp signature_template.xml"
+doc = XML::Parser.file("intermediate_files/signed.xml").parse
 
 # clear out the CipherValues
 xenc = "xenc:http://www.w3.org/2001/04/xmlenc#"
@@ -46,17 +46,16 @@ doc.find("//xenc:CipherValue", xenc).each { |cv| cv.content="" }
 
 # encrypt the file
 doc.save("signed.xml", :indent => true, :encoding => XML::Encoding::UTF_8)
-sh "xmlsec1 encrypt --pubkey-cert-pem vbms.cms.test.vbms.aide.oit.va.gov.crt --pwd importkey --session-key des-192 --xml-data signed.xml  --output encrypted.xml --node-xpath /soapenv:Envelope/soapenv:Body/* session-key-template.xml"
-
-doc = XML::Parser.file("encrypted.xml").parse
+sh "xmlsec1 encrypt --pubkey-cert-pem vbms.cms.test.vbms.aide.oit.va.gov.crt --pwd importkey --session-key des-192 --xml-data signed.xml  --output intermediate_files/encrypted.xml --node-xpath /soapenv:Envelope/soapenv:Body/* session-key-template.xml"
+doc = XML::Parser.file("intermediate_files/encrypted.xml").parse
 
 # inject the saml token
 saml = doc.import(XML::Parser.file("samlToken-cui-tst.xml").parse.root)
 wsse = "wsse:http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
 doc.find_first("//wsse:Security", wsse) << saml
 
-doc.save("encrypted_saml.xml", :indent => true, :encoding => XML::Encoding::UTF_8)
-doc = XML::Parser.file("encrypted_saml.xml").parse
+doc.save("intermediate_files/encrypted_saml.xml", :indent => true, :encoding => XML::Encoding::UTF_8)
+doc = XML::Parser.file("intermediate_files/encrypted_saml.xml").parse
 
 url = "https://filenet.test.vbms.aide.oit.va.gov/vbmsp2-cms/streaming/eDocumentService-v4?WSDL"
 client = Savon.client(wsdl: url, ssl_verify_mode: :none)#, ssl_cert_file: "vbms_test_public_key.crt")
