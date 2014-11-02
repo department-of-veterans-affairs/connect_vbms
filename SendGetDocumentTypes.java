f59dd4b6ab8abe535fbf086d9775251bc455e4f6
Bill Mill <bill.mill@gmail.com>
2014-11-01 22:42:11 -0400

    merge upstream

--- SendGetDocumentTypes.java
+++ SendGetDocumentTypes.java
@@ -1,20 +1,15 @@
 
 import org.apache.ws.security.message.WSSecHeader;
-import org.apache.ws.security.message.WSSecSignature;
 import org.apache.ws.security.message.WSSecTimestamp;
 import org.apache.ws.security.util.*;
 import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.apache.ws.security.components.crypto.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.nio.charset.Charset;
-
 
 public class SendGetDocumentTypes 
 {
@@ -23,56 +18,22 @@ public class SendGetDocumentTypes
   {
     try 
     {
-      List<String> lines = Files.readAllLines(Paths.get("getDocumentTypes.xml"), Charset.defaultCharset());
-      String document = "";
-      for (String line : lines)
-      {
-        document += line;
-      }
-      
-      document = addTimestamp(document);
-      document = addSignature(document);
-      
-      System.out.println(document);
+      InputStream in = new ByteArrayInputStream(Files.readAllBytes(Paths.get("getDocumentTypes.xml")));
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      factory.setNamespaceAware(true);
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      Document doc = builder.parse(in);
+      WSSecHeader secHeader = new WSSecHeader();
+      secHeader.insertSecurityHeader(doc);
+      WSSecTimestamp timestamp = new WSSecTimestamp();
+      timestamp.setTimeToLive(300);
+      Document createdDoc = timestamp.build(doc, secHeader);
+      String outputString = XMLUtils.PrettyDocumentToString(createdDoc);
+      System.out.println(outputString);
     }
     catch (Exception e)
     {
       System.out.println(e);
     }
-  }
-  
-  public static Document getSOAPDoc(String document) throws Exception
-  {
-    InputStream in = new ByteArrayInputStream(document.getBytes());
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    factory.setNamespaceAware(true);
-    DocumentBuilder builder = factory.newDocumentBuilder();
-    Document doc = builder.parse(in);
-    return doc;
-  }
-  
-  public static String addTimestamp(String document) throws Exception
-  {
-    Document doc = getSOAPDoc(document);
-    WSSecHeader secHeader = new WSSecHeader();
-    secHeader.insertSecurityHeader(doc);
-    WSSecTimestamp timestamp = new WSSecTimestamp();
-    timestamp.setTimeToLive(300);
-    Document createdDoc = timestamp.build(doc, secHeader);
-    String outputString = XMLUtils.PrettyDocumentToString(createdDoc);
-    return outputString;
-  }
-  
-  public static String addSignature(String document) throws Exception
-  {
-    Crypto crypto = CryptoFactory.getInstance();
-    WSSecSignature builder = new WSSecSignature();
-    builder.setUserInfo("importkey", "importkey");
-    Document doc = getSOAPDoc(document);
-    WSSecHeader secHeader = new WSSecHeader();
-    secHeader.insertSecurityHeader(doc);
-    Document signedDoc = builder.build(doc, crypto, secHeader);
-    String outputString = XMLUtils.PrettyDocumentToString(signedDoc);
-    return outputString;  
   }
 }
