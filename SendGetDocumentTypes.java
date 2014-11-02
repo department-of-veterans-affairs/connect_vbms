5273145846d406b74571e46d5e6f201e646223a3
Bill Mill <bill.mill@gmail.com>
2014-11-02 00:14:26 -0400

    merge upstream

--- SendGetDocumentTypes.java
+++ SendGetDocumentTypes.java
@@ -1,8 +1,10 @@
 import org.apache.ws.security.message.WSSecEncrypt;
 import org.apache.ws.security.message.WSSecHeader;
+import org.apache.ws.security.message.WSSecSAMLToken;
 import org.apache.ws.security.message.WSSecSignature;
 import org.apache.ws.security.message.WSSecTimestamp;
 import org.apache.ws.security.util.*;
+import org.apache.ws.security.saml.*;
 import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,6 +16,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.nio.charset.Charset;
+import org.apache.ws.security.WSEncryptionPart;
+import java.util.ArrayList;
+import org.apache.ws.security.SOAPConstants;
+import org.apache.ws.security.WSConstants;
 
 
 public class SendGetDocumentTypes 
@@ -31,14 +37,17 @@ public class SendGetDocumentTypes
       }
       
       document = addTimestamp(document);
-      document = addSignature(document);
-      document = addEncryption(document);
-      
       System.out.println(document);
+      document = addSignature(document);
+      System.out.println(document);
+      document = addEncryption(document);
+      //document = addSAMLToken(document);
+      
+      //System.out.println(document);
     }
     catch (Exception e)
     {
-      System.out.println(e);
+      e.printStackTrace();
     }
   }
   
@@ -70,11 +79,20 @@ public class SendGetDocumentTypes
     WSSecSignature builder = new WSSecSignature();
     builder.setUserInfo("importkey", "importkey");
     Document doc = getSOAPDoc(document);
+    SOAPConstants soapConstants = WSSecurityUtil.getSOAPConstants(doc.getDocumentElement());
     WSSecHeader secHeader = new WSSecHeader();
+    secHeader.setMustUnderstand(false);
     secHeader.insertSecurityHeader(doc);
+    List<WSEncryptionPart> references = new ArrayList<WSEncryptionPart>();
+    references.add(new WSEncryptionPart("TS-1"));
+    String soapNamespace = WSSecurityUtil.getSOAPNamespace(doc.getDocumentElement());
+    WSEncryptionPart bodyPart = new WSEncryptionPart(WSConstants.ELEM_BODY, WSSecurityUtil.getSOAPNamespace(doc.getDocumentElement()), "Content");
+    references.add(bodyPart);
+    //builder.addReferencesToSign(references, secHeader);
+    builder.setParts(references);
     Document signedDoc = builder.build(doc, crypto, secHeader);
     String outputString = XMLUtils.PrettyDocumentToString(signedDoc);
-    return outputString;  
+    return outputString;
   }
   
   public static String addEncryption(String document) throws Exception
@@ -89,4 +107,16 @@ public class SendGetDocumentTypes
     String outputString = XMLUtils.PrettyDocumentToString(encryptedDoc);
     return outputString;
   }
+  
+  /*public static String addSAMLToken(String document) throws Exception
+  {
+    WSSecSAMLToken wsSign = new WSSecSAMLToken();
+    Document doc = getSOAPDoc(document);
+    WSSecHeader secHeader = new WSSecHeader();
+    secHeader.insertSecurityHeader(doc);
+    Document tokenedDoc = wsSign.build(doc, samlAssertion, secHeader);
+    String outputString = XMLUtils.PrettyDocumentToString(tokenedDoc);
+    return outputString;    
+  } 
+    */   
 }
