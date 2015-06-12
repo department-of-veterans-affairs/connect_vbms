7b744c64ff40a90d04bbad9d8fd790971d70ee99
Bill Mill <bill@billmill.org>
2015-06-12 15:08:12 -0400

    move DBLogger and pg require back into send.rb

--- src/send.rb
+++ src/send.rb
@@ -4,6 +4,50 @@ require 'optparse'
 
 require_relative 'vbms'
 
+# Once connect_vbms is a gem, all the code in this file should go into CUI or
+# be deleted, and this file should be deleted
+
+if ENV.has_key? "CONNECT_VBMS_POSTGRES"
+  begin
+    require 'pg'
+  rescue LoadError
+    print <<-EOF
+Unable to load the 'pg' gem, which is required if the CONNECT_VBMS_POSTGRES
+environment variable is set. Please either install the 'pg' gem or unset
+the CONNECT_VBMS_POSTGRES environment variable.
+    EOF
+    raise
+  end
+end
+
+class DBLogger
+  def initialize(pg_uri)
+    uri = URI.parse(pg_uri)
+    @conn = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  end
+
+  def log(event, data)
+    if event == :decrypted_message
+      message = "connect_vbms decrypted response"
+      request_body = ""
+      response_body = data[:decrypted_data]
+      evaluation_id = data[:request].file_number
+    elsif event == :request
+      message = "connect_vbms status #{data[:response_code]}"
+      request_body = data[:request_body]
+      response_body = data[:response_body]
+      evaluation_id = data[:request].file_number
+    else
+      raise NotImplementedError.new(event)
+    end
+
+    @conn.exec_params(<<-EOM, [message, request_body, response_body, evaluation_id])
+INSERT INTO external_activity_logs(message, submitted_data, response_body, evaluation_id)
+VALUES ($1, $2, $3, $4)
+EOM
+  end
+end
+
 def parse(args)
   usage = "Usage: send.rb --pdf <filename> --env <env> --file_number <n> --received_dt <dt> --first_name <name> --middle_name [<name>] --last_name <name> --logfile [<file>] "
   options = {}
--- src/vbms.rb
+++ src/vbms.rb
@@ -9,17 +9,3 @@ require 'nokogiri'
 require 'vbms/common'
 require 'vbms/client'
 require 'vbms/requests'
-require 'vbms/db_logger'
-
-if ENV.has_key? "CONNECT_VBMS_POSTGRES"
-  begin
-    require 'pg'
-  rescue LoadError
-    print <<-EOF
-Unable to load the 'pg' gem, which is required if the CONNECT_VBMS_POSTGRES
-environment variable is set. Please either install the 'pg' gem or unset
-the CONNECT_VBMS_POSTGRES environment variable.
-    EOF
-    raise
-  end
-end
