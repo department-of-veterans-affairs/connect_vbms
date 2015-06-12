58c2e0c94967937d030505bc90af504823a91da3
Bill Mill <bill@billmill.org>
2015-06-12 14:20:56 -0400

    remove DBLogger and db_log from send.rb

--- src/send.rb
+++ src/send.rb
@@ -4,42 +4,6 @@ require 'optparse'
 
 require_relative 'vbms'
 
-
-# log to the DrTurboTax external_activity_log
-def db_log(conn, message, request_body, response_body, evaluation_id)
-  conn.exec_params(<<-EOM, [message, request_body, response_body, evaluation_id])
-INSERT INTO external_activity_logs(message, submitted_data, response_body, evaluation_id)
-VALUES ($1, $2, $3, $4)
-  EOM
-end
-
-class DBLogger
-  def initialize(conn)
-    @conn = conn
-  end
-
-  def log(event, data)
-    if event == :decrypted_message
-      message = "connect_vbms decrypted response"
-      request_body = ""
-      response_body = data[:decrypted_data]
-      evaluation_id = data[:request].file_number
-    elsif event == :request
-      message = "connect_vbms status #{data[:response_code]}"
-      request_body = data[:request_body]
-      response_body = data[:response_body]
-      evaluation_id = data[:request].file_number
-    else
-      raise NotImplementedError.new(event)
-    end
-
-    @conn.exec_params(<<-EOM, [message, request_body, response_body, evaluation_id])
-INSERT INTO external_activity_logs(message, submitted_data, response_body, evaluation_id)
-VALUES ($1, $2, $3, $4)
-EOM
-  end
-end
-
 def parse(args)
   usage = "Usage: send.rb --pdf <filename> --env <env> --file_number <n> --received_dt <dt> --first_name <name> --middle_name [<name>] --last_name <name> --logfile [<file>] "
   options = {}
