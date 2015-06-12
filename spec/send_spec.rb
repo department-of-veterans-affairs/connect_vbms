f0c9debfdc3356fe00b67a97b00cb204a40a28d7
Bill Mill <bill@billmill.org>
2015-06-12 14:38:08 -0400

    be more careful with CONNECT_VBMS_POSTGRES

--- spec/send_spec.rb
+++ spec/send_spec.rb
@@ -5,16 +5,30 @@ require 'byebug'
 require 'send'
 
 RSpec.describe 'send.rb' do
-  it "should initialize a postgres logger if CONNECT_VBMS_POSTGRES is set" do
-    ENV["CONNECT_VBMS_POSTGRES"] = "postgres://localhost/drturbotax_development"
-    expect(PG).to receive(:connect).with('localhost', nil, nil, nil, 'drturbotax_development', nil, nil)
-    logger = init_logger
-    expect(logger.class).to eq VBMS::DBLogger
-    ENV.delete "CONNECT_VBMS_POSTGRES"
+  context "when CONNECT_VBMS_POSTGRES is set" do
+    before do
+      ENV["CONNECT_VBMS_POSTGRES"] = "postgres://localhost/drturbotax_development"
+    end
+
+    after do
+      ENV.delete "CONNECT_VBMS_POSTGRES"
+    end
+
+    it "should initialize a postgres logger if CONNECT_VBMS_POSTGRES is set" do
+      expect(PG).to receive(:connect).with('localhost', nil, nil, nil, 'drturbotax_development', nil, nil)
+      logger = init_logger
+      expect(logger.class).to eq VBMS::DBLogger
+    end
   end
 
-  it "should return nil if CONNECT_VBMS_POSTGRES is not set" do
-    logger = init_logger
-    expect(logger.class).to eq NilClass
+  context "when CONNECT_VBMS_POSTGRES is not set" do
+    before do
+      ENV.delete "CONNECT_VBMS_POSTGRES"
+    end
+
+    it "should return nil if CONNECT_VBMS_POSTGRES is not set" do
+      logger = init_logger
+      expect(logger.class).to eq NilClass
+    end
   end
 end
