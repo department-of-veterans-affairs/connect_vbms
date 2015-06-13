8574966443c78708f2a5c4f83fc3486af6581957
Albert J. Wong <awong.dev@gmail.com>
2015-06-12 23:11:10 -0700

    Add stub test files for doing crypto in ruby.
    Since Java is the reference implementation, the easiest initial test
    should be insuring interoperability between our system and the reference
    code.
    
    This adds the stubs for creating such a system.

---
+++ spec/ruby_crypto_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "Ruby Encrypt/Decrypt test vs Java reference impl" do
+  it "takes a cleartext soap message, encrypts in ruby, and decrypts using java" do
+  end
+
+  it "takes a cleartext soap message, encrypts in java, and decrypts using ruby" do
+  end
+end
