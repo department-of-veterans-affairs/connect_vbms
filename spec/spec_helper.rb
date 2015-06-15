f838ac591becf24be4b78479c6cc420bfac10d41
Bill Mill <bill@billmill.org>
2015-06-15 10:47:09 -0400

    remove unused function from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,14 +5,6 @@ require 'vbms'
 require 'nokogiri'
 require 'byebug'
 
-def get_env(env_var_name, allow_empty=false)
-  value = ENV[env_var_name]
-  if not allow_empty || value
-    raise "#{env_var_name} must be set"
-  end
-  value
-end
-
 def env_path(env_dir, env_var_name)
   value = ENV[env_var_name]
   if value.nil?
