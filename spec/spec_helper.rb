7d20e8262ccf0a78ddb2ec3987f5012e9f865023
Bill Mill <bill@billmill.org>
2015-06-15 10:32:44 -0400

    add spec_helper for common stuff

---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+# TODO: remove this once we can put our source code in `lib/`
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "src")
+
+require 'vbms'
+require 'nokogiri'
+require 'byebug'
+
+def get_env(env_var_name, allow_empty=false)
+  value = ENV[env_var_name]
+  if not allow_empty || value
+    raise "#{env_var_name} must be set"
+  end
+  value
+end
+
+def env_path(env_dir, env_var_name)
+  value = ENV[env_var_name]
+  if value.nil?
+    return nil
+  else
+    return File.join(env_dir, value)
+  end
+end
+
+RSpec.configure do |config|
+# The settings below are suggested to provide a good initial experience
+# with RSpec, but feel free to customize to your heart's content.
+
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
