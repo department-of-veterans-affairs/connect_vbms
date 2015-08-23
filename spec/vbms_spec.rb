ebc7a7adcc12b89b68bf0b0eaf65ad6b57d07889
Jeff Maher <jeff.maher@va.gov>
2015-08-23 12:55:56 -0400

    Remove commented out test

--- spec/vbms_spec.rb
+++ spec/vbms_spec.rb
@@ -174,17 +174,6 @@ describe VBMS::Requests do
 
       @client.send(request)
     end
-
-    # it "parses received dates correctly" do
-    #   request = VBMS::Requests::ListDocuments.new('')
-    #   xml = File.read(list_documents_response_xml)
-    #   doc = Nokogiri::XML(xml)
-    #   vbmsDocs = request.handle_response(doc)
-    #
-    #   expect(vbmsDocs[0].received_at).to eq(Date.parse('2015-08-03-04:00'))
-    #   expect(vbmsDocs[1].received_at).to eq(Date.parse('2015-08-07-04:00'))
-    # end
-
   end
 
   describe "FetchDocumentById" do
