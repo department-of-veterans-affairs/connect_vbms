1ba339f1df1cad2337f866a31543abe7c30bdb2f
Jeff Maher <jeff.maher@va.gov>
2015-08-23 12:50:51 -0400

    Moved ListDocuments test into separate file

--- spec/vbms_spec.rb
+++ spec/vbms_spec.rb
@@ -210,17 +210,3 @@ describe VBMS::Requests do
     end
   end
 end
-
-describe VBMS::Requests::ListDocuments do
-  let (:list_documents_response_xml) { fixture_path('list_documents_response.xml') }
-
-  it "parses received dates correctly" do
-    request = VBMS::Requests::ListDocuments.new('')
-    xml = File.read(list_documents_response_xml)
-    doc = Nokogiri::XML(xml)
-    vbmsDocs = request.handle_response(doc)
-
-    expect(vbmsDocs[0].received_at).to eq(Date.parse('2015-08-03-04:00'))
-    expect(vbmsDocs[1].received_at).to eq(Date.parse('2015-08-07-04:00'))
-  end
-end
