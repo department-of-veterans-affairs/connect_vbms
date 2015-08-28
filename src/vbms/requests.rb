8661aa14dfd07c79a354831fa01f0361df005363
Bill Mill <bill@billmill.org>
2015-08-27 21:32:50 -0400

    make namespaces a module-level constant

--- src/vbms/requests.rb
+++ src/vbms/requests.rb
@@ -1,16 +1,16 @@
 module VBMS
   module Requests
-    def self.soap
-      namespaces = {
-        "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
-        "xmlns:v4" => "http://vbms.vba.va.gov/external/eDocumentService/v4",
-        "xmlns:doc" => "http://vbms.vba.va.gov/cdm/document/v4",
-        "xmlns:cdm" => "http://vbms.vba.va.gov/cdm",
-        "xmlns:xop" => "http://www.w3.org/2004/08/xop/include",
-      }
+    NAMESPACES = {
+      "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
+      "xmlns:v4" => "http://vbms.vba.va.gov/external/eDocumentService/v4",
+      "xmlns:doc" => "http://vbms.vba.va.gov/cdm/document/v4",
+      "xmlns:cdm" => "http://vbms.vba.va.gov/cdm",
+      "xmlns:xop" => "http://www.w3.org/2004/08/xop/include",
+    }
 
+    def self.soap
       Nokogiri::XML::Builder.new do |xml|
-        xml['soapenv'].Envelope(namespaces) {
+        xml['soapenv'].Envelope(VBMS::Requests::NAMESPACES) {
           xml['soapenv'].Header
           xml['soapenv'].Body { yield(xml) }
         }
