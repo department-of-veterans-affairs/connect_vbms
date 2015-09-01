1a1a17e512bcaedc23d39160f73a4219bc722ec9
Jacob Harris <jacob.harris@gsa.gov>
2015-09-01 13:51:35 -0400

    Initial versions of unit tests for each request class

--- spec/requests/fetch_document_by_id_spec.rb
+++ spec/requests/fetch_document_by_id_spec.rb
@@ -1,14 +1,45 @@
 require 'spec_helper'
 
 describe VBMS::Requests::FetchDocumentById do
-  subject { VBMS::Requests::FetchDocumentById.new("a document id") }
+  describe "render_xml" do
+    subject { VBMS::Requests::FetchDocumentById.new("a document id") }
 
-  it "generates valid XML" do
-    xml = subject.render_xml
-    doc = Nokogiri::XML::Document.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-    xsd = Nokogiri::XML::Schema(File.read("spec/soap.xsd"))
-    expect(xsd.errors).to eq []
-    errors = xsd.validate(Nokogiri::XML(xml))
-    expect(errors).to eq []
+    it "generates valid XML" do
+      xml = subject.render_xml
+      doc = Nokogiri::XML::Document.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
+      xsd = Nokogiri::XML::Schema(File.read("spec/soap.xsd"))
+      expect(xsd.errors).to eq []
+      errors = xsd.validate(Nokogiri::XML(xml))
+      expect(errors).to eq []
+    end
+  end
+
+  describe "parsing the XML" do
+    before(:all) do
+      request = VBMS::Requests::FetchDocumentById.new('')
+      xml = File.read(fixture_path('requests/fetch_document.xml'))
+      doc = Nokogiri::XML(xml)
+      @response = request.handle_response(doc)
+    end
+
+    subject { @response }
+
+    it "should return a DocumentWithContent object" do
+      expect(subject).to be_a(VBMS::DocumentWithContent)
+    end
+
+    it "has valid document information" do
+      doc = subject.document
+
+      expect(doc.document_id).to eq('{9E364101-AFDD-49A7-A11F-602CCF2E5DB5}')
+      expect(doc.filename).to eq('tmp20150506-94244-6zotzp')
+      expect(doc.doc_type).to eq('356')
+      expect(doc.source).to eq('VHA_CUI')
+      expect(doc.received_at).to eq(Time.parse('2015-05-06-04:00').to_date)
+    end
+
+    it "has some associated content" do
+      expect(subject.content).to_not be_nil
+    end
   end
 end
--- spec/requests/upload_document_with_associations_spec.rb
+++ spec/requests/upload_document_with_associations_spec.rb
@@ -1,22 +1,40 @@
 require 'spec_helper'
 
 describe VBMS::Requests::UploadDocumentWithAssociations do
-  subject { VBMS::Requests::UploadDocumentWithAssociations.new(
-    "123456788",
-    Time.now.utc,
-    "Joe", "Eagle", "Citizen",
-    "UDWA test exam name",
-    "/pdf/does/not/exist",
-    "doc_type",
-    "UDWA test source",
-    "UDWA new mail") }
+  describe "render_xml" do
+    subject { VBMS::Requests::UploadDocumentWithAssociations.new(
+      '123456788',
+      Time.now.utc,
+      'Joe', 'Eagle', 'Citizen',
+      'UDWA test exam name',
+      '/pdf/does/not/exist',
+      'doc_type',
+      'UDWA test source',
+      'UDWA new mail') }
 
-  it "generates valid XML" do
-    xml = subject.render_xml
-    doc = Nokogiri::XML::Document.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-    xsd = Nokogiri::XML::Schema(File.read("spec/soap.xsd"))
-    expect(xsd.errors).to eq []
-    errors = xsd.validate(Nokogiri::XML(xml))
-    expect(errors).to eq []
+    it "generates valid XML" do
+      xml = subject.render_xml
+      doc = Nokogiri::XML::Document.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
+      xsd = Nokogiri::XML::Schema(File.read("spec/soap.xsd"))
+      expect(xsd.errors).to eq []
+      errors = xsd.validate(Nokogiri::XML(xml))
+      expect(errors).to eq []
+    end
+  end
+
+  describe "parsing the XML" do
+    before(:all) do
+      request = VBMS::Requests::FetchDocumentById.new('')
+      xml = File.read(fixture_path('requests/upload_document_with_associations.xml'))
+      doc = Nokogiri::XML(xml)
+      @response = request.handle_response(doc)
+    end
+
+    subject { @response }
+
+    # TODO: should we do more with this?
+    it "should just return the document" do
+      expect(subject).to eq(doc)
+    end
   end
 end
