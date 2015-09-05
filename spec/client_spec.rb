edb6fca22e8422339d245a007f03ebe43218f0a8
Bill Mill <bill@billmill.org>
2015-09-04 21:45:45 -0400

    revert spec changes to satisfy rubocop

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,17 +7,15 @@ describe VBMS::Client do
     )
   end
 
-  describe 'remove_must_understand' do
-    it 'takes a Nokogiri document and deletes the mustUnderstand attribute' do
+  describe "remove_must_understand" do
+    it "takes a Nokogiri document and deletes the mustUnderstand attribute" do
       doc = Nokogiri::XML(<<-EOF)
       <?xml version="1.0" encoding="UTF-8"?>
-      <soapenv:Envelope 
-           xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-           xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-        <soapenv:Header>
-          <wsse:Security soapenv:mustUnderstand="1">
-          </wsse:Security>
-        </soapenv:Header>
+      <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+          <soapenv:Header>
+              <wsse:Security soapenv:mustUnderstand="1">
+              </wsse:Security>
+          </soapenv:Header>
       </soapenv:Envelope>
       EOF
 
@@ -27,31 +25,30 @@ describe VBMS::Client do
     end
   end
 
-  describe '#send' do
+  describe "#send" do
     before do
       @client = VBMS::Client.new(
         'http://test.endpoint.url/', nil, nil, nil, nil, nil, nil
       )
-
-      @request = double('request',
-                        file_number: '123456788',
-                        received_at: DateTime.new(2010, 01, 01),
-                        first_name: 'Joe',
-                        middle_name: 'Eagle',
-                        last_name: 'Citizen',
-                        exam_name: 'Test Fixture Exam',
-                        pdf_file: '',
-                        doc_type: '',
-                        source: 'CUI tests',
-                        name: 'uploadDocumentWithAssociations',
-                        new_mail: '',
-                        render_xml: '<xml></xml>')
-
-      @response = double('response', code: 200, body: 'response')
+      @request = double("request",
+        file_number: "123456788",
+        received_at: DateTime.new(2010, 01, 01),
+        first_name: "Joe",
+        middle_name: "Eagle",
+        last_name: "Citizen",
+        exam_name: "Test Fixture Exam",
+        pdf_file: "",
+        doc_type: "",
+        source: "CUI tests",
+        name: "uploadDocumentWithAssociations",
+        new_mail: "",
+        render_xml: "<xml></xml>",
+      )
+      @response = double("response", :code => 200, :body => "response")
     end
 
-    it 'creates two log messages' do
-      body = Nokogiri::XML('<xml>body</xml>')
+    it "creates two log messages" do
+      body = Nokogiri::XML("<xml>body</xml")
       allow(HTTPI).to receive(:post).and_return(@response)
       allow(@client).to receive(:process_response).and_return(nil)
       allow(VBMS).to receive(:encrypted_soap_document_xml).and_return(body.to_s)
@@ -61,83 +58,80 @@ describe VBMS::Client do
       allow(@client).to receive(:process_body)
 
       expect(@client).to receive(:log).with(:unencrypted_xml, unencrypted_body: @request.render_xml)
-      expect(@client).to receive(:log).with(:request, response_code: @response.code, 
-                                                      request_body: body.to_s, 
-                                                      response_body: @response.body, 
-                                                      request: @request)
+      expect(@client).to receive(:log).with(:request, response_code: @response.code, request_body: body.to_s, response_body: @response.body, request: @request)
 
       @client.send(@request)
     end
   end
 
-  describe 'from_env_vars' do
-    let(:vbms_env_vars) do
-      { 'CONNECT_VBMS_ENV_DIR' => '/my/path/to/credentials',
+  describe "from_env_vars" do
+  let (:vbms_env_vars) { {
+        'CONNECT_VBMS_ENV_DIR' => '/my/path/to/credentials',
         'CONNECT_VBMS_URL' => 'http://example.com/fake_vbms',
         'CONNECT_VBMS_KEYFILE' => 'fake_keyfile.some_ext',
         'CONNECT_VBMS_SAML' => 'fake_saml_token',
         'CONNECT_VBMS_KEY' => 'fake_keyname',
         'CONNECT_VBMS_KEYPASS' => 'fake_keypass',
         'CONNECT_VBMS_CACERT' => 'fake_cacert',
-        'CONNECT_VBMS_CERT' => 'fake_cert' }
-    end
+        'CONNECT_VBMS_CERT' => 'fake_cert',
+      } }
 
-    it 'smoke test that it initializes when all environment variables are set' do
+    it "smoke test that it initializes when all environment variables are set" do
       stub_const('ENV', vbms_env_vars)
       expect(VBMS::Client.from_env_vars).not_to be_nil
     end
 
-    describe 'required environment variables' do
-      it 'needs CONNECT_VBMS_ENV_DIR set' do
+    describe "required environment variables" do
+      it "needs CONNECT_VBMS_ENV_DIR set" do
         vbms_env_vars.delete('CONNECT_VBMS_ENV_DIR')
         stub_const('ENV', vbms_env_vars)
-        expect { VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
-                                                             /CONNECT_VBMS_ENV_DIR must be set/)
+        expect{ VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
+                                                            /CONNECT_VBMS_ENV_DIR must be set/)
       end
 
-      it 'needs CONNECT_VBMS_URL set' do
+      it "needs CONNECT_VBMS_URL set" do
         vbms_env_vars.delete('CONNECT_VBMS_URL')
         stub_const('ENV', vbms_env_vars)
-        expect { VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
-                                                             /CONNECT_VBMS_URL must be set/)
+        expect{ VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
+                                                            /CONNECT_VBMS_URL must be set/)
       end
 
-      it 'needs CONNECT_VBMS_KEYFILE set' do
+      it "needs CONNECT_VBMS_KEYFILE set" do
         vbms_env_vars.delete('CONNECT_VBMS_KEYFILE')
         stub_const('ENV', vbms_env_vars)
-        expect { VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
-                                                             /CONNECT_VBMS_KEYFILE must be set/)
+        expect{ VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
+                                                            /CONNECT_VBMS_KEYFILE must be set/)
       end
 
-      it 'needs CONNECT_VBMS_SAML set' do
+      it "needs CONNECT_VBMS_SAML set" do
         vbms_env_vars.delete('CONNECT_VBMS_SAML')
         stub_const('ENV', vbms_env_vars)
-        expect { VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
-                                                             /CONNECT_VBMS_SAML must be set/)
+        expect{ VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
+                                                            /CONNECT_VBMS_SAML must be set/)
       end
 
-      it 'needs CONNECT_VBMS_KEYPASS set' do
+      it "needs CONNECT_VBMS_KEYPASS set" do
         vbms_env_vars.delete('CONNECT_VBMS_KEYPASS')
         stub_const('ENV', vbms_env_vars)
-        expect { VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
-                                                             /CONNECT_VBMS_KEYPASS must be set/)
+        expect{ VBMS::Client.from_env_vars }.to raise_error(VBMS::EnvironmentError,
+                                                            /CONNECT_VBMS_KEYPASS must be set/)
       end
     end
 
-    describe 'required environment variables' do
-      it 'needs CONNECT_VBMS_KEY set' do
+    describe "required environment variables" do
+      it "needs CONNECT_VBMS_KEY set" do
         vbms_env_vars.delete('CONNECT_VBMS_KEY')
         stub_const('ENV', vbms_env_vars)
         expect(VBMS::Client.from_env_vars).not_to be_nil
       end
 
-      it 'needs CONNECT_VBMS_CACERT set' do
+      it "needs CONNECT_VBMS_CACERT set" do
         vbms_env_vars.delete('CONNECT_VBMS_CACERT')
         stub_const('ENV', vbms_env_vars)
         expect(VBMS::Client.from_env_vars).not_to be_nil
       end
 
-      it 'needs CONNECT_VBMS_CERT set' do
+      it "needs CONNECT_VBMS_CERT set" do
         vbms_env_vars.delete('CONNECT_VBMS_CERT')
         stub_const('ENV', vbms_env_vars)
         expect(VBMS::Client.from_env_vars).not_to be_nil
@@ -186,11 +180,7 @@ describe VBMS::Client do
       end
 
       context "when it is given a document that won't decrypt" do
-        let(:response_body) do
-          encrypted_xml_file(fixture_path('requests/fetch_document.xml'), 
-                             'fetchDocumentResponse').gsub(%r{<xenc:CipherValue>.+</xenc:CipherValue>}, 
-                                                           '<xenc:CipherValue></xenc:CipherValue>')
-        end
+        let(:response_body) { encrypted_xml_file(fixture_path('requests/fetch_document.xml'), 'fetchDocumentResponse').gsub(%r{<xenc:CipherValue>.+</xenc:CipherValue>}, '<xenc:CipherValue></xenc:CipherValue>') }
 
         it "should raise a SOAPError" do
           expect { subject }.to raise_error do |error|
@@ -202,8 +192,7 @@ describe VBMS::Client do
       end
 
       context "when it is given a document that contains a SOAP fault" do
-        let(:response_body) do 
-          <<-EOF
+        let(:response_body) do <<-EOF
           <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
             <soap:Header/>
             <soap:Body>
@@ -228,16 +217,7 @@ describe VBMS::Client do
       end
 
       context 'when the server sends an HTML response error page' do
-        let(:response_body) do 
-          <<-EOF
-          <html>
-            <head><title>An error has occurred</title></head>
-            <body>
-              <p>I know you were expecting HTML, but sometimes sites do this</p>
-            </body>
-          </html>
-          EOF
-        end
+        let(:response_body) { "<html><head><title>An error has occurred</title></head><body><p>I know you were expecting HTML, but sometimes sites do this</p></body></html>"}
 
         it "should raise a SOAPError" do
           expect { subject }.to raise_error do |error|
