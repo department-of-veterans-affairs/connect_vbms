b027f44cdfa3786ec83d38f70407cdd3b75c9fca
Bill Mill <bill@billmill.org>
2015-09-04 21:53:28 -0400

    revert rubocop changes to spec/requests files

--- spec/requests/upload_document_with_associations_spec.rb
+++ spec/requests/upload_document_with_associations_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe VBMS::Requests::UploadDocumentWithAssociations do
-  subject do 
-    VBMS::Requests::UploadDocumentWithAssociations.new(
+  subject { VBMS::Requests::UploadDocumentWithAssociations.new(
       '123456788',
       Time.now.utc,
       'Joe', 'Eagle', 'Citizen',
@@ -10,12 +9,12 @@ describe VBMS::Requests::UploadDocumentWithAssociations do
       '/pdf/does/not/exist',
       'doc_type',
       'UDWA test source',
-      'UDWA new mail')
-  end
+      'UDWA new mail') }
 
   describe "render_xml" do
     it "generates valid XML" do
       xml = subject.render_xml
+      doc = Nokogiri::XML::Document.parse(xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
       xsd = Nokogiri::XML::Schema(File.read("spec/soap.xsd"))
       expect(xsd.errors).to eq []
       errors = xsd.validate(Nokogiri::XML(xml))
@@ -30,6 +29,7 @@ describe VBMS::Requests::UploadDocumentWithAssociations do
       @response = subject.handle_response(@doc)
     end
 
+    # TODO: should we do more with this?
     it "should just return the document" do
       expect(@response).to eq(@doc)
     end
