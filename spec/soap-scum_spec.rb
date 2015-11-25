53b6b4afa7efd5af851de595f350916f52f33aa1
amoose <amos@amosarts.com>
2015-11-25 08:23:33 -0800

    Adds failing spec for java:ruby comparison

--- spec/soap-scum_spec.rb
+++ spec/soap-scum_spec.rb
@@ -10,11 +10,11 @@ describe :SoapScum do
   let (:server_cert) { fixture_path('server.crt') }
 
   let (:test_jks_keystore) { fixture_path('test_keystore.jks') }
-  let (:test_keystore_pass) { "importkey" }
+  let (:test_keystore_pass) { 'importkey' }
   let (:keypass) { 'importkey' }
 
-  describe "KeyStore" do
-    it "loads a pc12 file and cert" do
+  describe 'KeyStore' do
+    it 'loads a pc12 file and cert' do
       keystore = SoapScum::KeyStore.new
       # keystore.add_pc12(server_pc12, keypass)
       skip
@@ -24,7 +24,7 @@ describe :SoapScum do
       expect(keystore.get_certificate(server_x509_subject).to_der).to eql(server_pkcs12.certificate.to_der)
     end
 
-    it "returns correct cert and key by subject" do
+    it 'returns correct cert and key by subject' do
       keystore = SoapScum::KeyStore.new
       # keystore.add_pc12(server_pc12, keypass)
       keystore.add_pc12(client_pc12, keypass)
@@ -50,52 +50,52 @@ describe :SoapScum do
     end
   end
 
-  describe "MessageProcessor" do
-    let (:keystore) {
+  describe 'MessageProcessor' do
+    let (:keystore) do
       keystore = SoapScum::KeyStore.new
       keystore.add_pc12(client_pc12, keypass)
       keystore.add_cert(server_cert)
       keystore
-    }
-    let (:crypto_options) {
+    end
+    let (:crypto_options) do
       {
         server: {
-            certificate: keystore.all.last.certificate,
-            keytransport_algorithm: SoapScum::MessageProcessor::CryptoAlgorithms::RSA_PKCS1_15,
-            cipher_algorithm: SoapScum::MessageProcessor::CryptoAlgorithms::AES128
-          },
+          certificate: keystore.all.last.certificate,
+          keytransport_algorithm: SoapScum::MessageProcessor::CryptoAlgorithms::RSA_PKCS1_15,
+          cipher_algorithm: SoapScum::MessageProcessor::CryptoAlgorithms::AES128
+        },
         client: {
-            certificate: keystore.all.first.certificate,
-            private_key: keystore.all.first.key,
-            digest_algorithm: "http://www.w3.org/2000/09/xmldsig#sha1",
-            signature_algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
-          }
+          certificate: keystore.all.first.certificate,
+          private_key: keystore.all.first.key,
+          digest_algorithm: 'http://www.w3.org/2000/09/xmldsig#sha1',
+          signature_algorithm: 'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
+        }
       }
-    }
-    let (:message_processor) {
+    end
+    let (:message_processor) do
       SoapScum::MessageProcessor.new(keystore)
-    }
-    let (:content_document) {
+    end
+    let (:content_document) do
       Nokogiri::XML('<hi-mom xmlns:example="http://example.com"><example:a-doc /></hi-mom>')
-    }
+    end
 
-    describe "#wrap_in_soap" do
-      it "creates a valid SOAP document" do
+    describe '#wrap_in_soap' do
+      it 'creates a valid SOAP document' do
         soap_doc = message_processor.wrap_in_soap(content_document)
         xsd = Nokogiri::XML::Schema(fixture('soap.xsd'))
         expect(xsd.validate(soap_doc).size).to eq(0)
       end
     end
 
-    describe "#encrypt" do
+    describe '#encrypt' do
       it 'returns valid SOAP' do
         soap_doc = message_processor.wrap_in_soap(content_document)
         ruby_encrypted_xml = message_processor.encrypt(soap_doc,
-                                                  'listDocuments',
-                                                  crypto_options,
-                                                  soap_doc.at_xpath(
-                                                    '/soapenv:Envelope/soapenv:Body',
-                                                    soapenv: SoapScum::XMLNamespaces::SOAPENV).children)
+                                                       'listDocuments',
+                                                       crypto_options,
+                                                       soap_doc.at_xpath(
+                                                         '/soapenv:Envelope/soapenv:Body',
+                                                         soapenv: SoapScum::XMLNamespaces::SOAPENV).children)
 
         xsd = Nokogiri::XML::Schema(fixture('soap.xsd'))
         doc = Nokogiri::XML(ruby_encrypted_xml)
@@ -103,29 +103,15 @@ describe :SoapScum do
         expect(xsd.validate(doc).size).to eq(0)
       end
 
-      it "creates an encrypted doc" do
+      it 'creates an encrypted soap doc' do
         soap_doc = message_processor.wrap_in_soap(content_document)
         ruby_encrypted_xml = message_processor.encrypt(soap_doc,
-                                                  'listDocuments',
-                                                  crypto_options,
-                                                  soap_doc.at_xpath(
-                                                    '/soapenv:Envelope/soapenv:Body',
-                                                    soapenv: SoapScum::XMLNamespaces::SOAPENV).children)
+                                                       'listDocuments',
+                                                       crypto_options,
+                                                       soap_doc.at_xpath(
+                                                         '/soapenv:Envelope/soapenv:Body',
+                                                         soapenv: SoapScum::XMLNamespaces::SOAPENV).children)
 
-        # java_encrypted_xml = VBMS.encrypted_soap_document_xml(soap_doc,
-        #                                           test_jks_keystore,
-        #                                           test_keystore_pass,
-        #                                           'listDocuments')
-        # decrypted_xml = VBMS.decrypt_message_xml(encrypted_xml, test_jks_keystore,
-        #                                          test_keystore_pass, 'test-logfile')
-
-        # encrypted_doc = Nokogiri::XML(encrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-        # decrypted_doc = Nokogiri::XML(decrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-
-        # parsed_ruby_xml = Nokogiri::XML(ruby_encrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-        # parsed_java_xml = Nokogiri::XML(java_encrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
-        # expect(parsed_ruby_xml.to_xml).to eq(parsed_java_xml.to_xml)
-        # expect(VBMS::Client)
         xsd = Nokogiri::XML::Schema(fixture('soap.xsd'))
         doc = Nokogiri::XML(ruby_encrypted_xml)
 
@@ -133,9 +119,26 @@ describe :SoapScum do
         # TODO(awong): Verify decrypt_xml matches original soap_doc.
       end
 
+      it 'encrypts the same as java' do
+        soap_doc = message_processor.wrap_in_soap(content_document)
+        ruby_encrypted_xml = message_processor.encrypt(soap_doc,
+                                                       'listDocuments',
+                                                       crypto_options,
+                                                       soap_doc.at_xpath(
+                                                         '/soapenv:Envelope/soapenv:Body',
+                                                         soapenv: SoapScum::XMLNamespaces::SOAPENV).children)
+        java_encrypted_xml = VBMS.encrypted_soap_document_xml(soap_doc,
+                                                              test_jks_keystore,
+                                                              test_keystore_pass,
+                                                              'listDocuments')
 
-      it "can be decrypted with ruby" do
-        skip "pending valid encryption and validation with java"
+        parsed_ruby_xml = Nokogiri::XML(ruby_encrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
+        parsed_java_xml = Nokogiri::XML(java_encrypted_xml, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
+        expect(parsed_ruby_xml.to_xml).to eq(parsed_java_xml.to_xml)
+      end
+
+      it 'can be decrypted with ruby' do
+        skip 'pending valid encryption and validation with java'
       end
     end
   end
