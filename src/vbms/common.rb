069c47947d4355bc012890c2540f3ec9be9cc500
Jacob Harris <jacob.harris@gsa.gov>
2015-11-27 11:32:18 -0500

    Add wsse utility namespace to common

--- src/vbms/common.rb
+++ src/vbms/common.rb
@@ -9,7 +9,8 @@ module VBMS
     v4: 'http://vbms.vba.va.gov/external/eDocumentService/v4',
     ns2: 'http://vbms.vba.va.gov/cdm/document/v4',
     soapenv: 'http://schemas.xmlsoap.org/soap/envelope/',
-    wsse: 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'
+    wsse: 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+    wsu: 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
   }
 
   class ClientError < StandardError
