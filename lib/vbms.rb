6159ee6bd1ec6cc4e76e89fd39d02fd22d075047
Shane Russell <shane.russell.va@gmail.com>
2016-11-23 15:16:58 -0500

    Add files

--- lib/vbms.rb
+++ lib/vbms.rb
@@ -26,6 +26,7 @@ require "vbms/requests/upload_document_with_associations"
 require "vbms/requests/list_documents"
 require "vbms/requests/fetch_document_by_id"
 require "vbms/requests/get_document_types"
+require "vbms/requests/establish_claim"
 
 # require 'xmldsig'
 require "xmldsig/signature_override"
