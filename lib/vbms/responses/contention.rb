a12f125455c0790090df17306ccbe01e400e8108
Shane Russell <shane.russell.va@gmail.com>
2017-11-21 11:46:17 -0500

    Clean it up

--- lib/vbms/responses/contention.rb
+++ lib/vbms/responses/contention.rb
@@ -1,24 +1,14 @@
 module VBMS
   module Responses
-    class Contention
-      attr_accessor :id, :text, :submit_date, :start_date
-  
-      def initialize(id:, text:, submit_date:, start_date:)
-        self.id = id
-        self.text = text
-        self.submit_date = submit_date
-        self.start_date = start_date
-      end
-  
+    class Contention < OpenStruct
       def self.create_from_xml(xml)
-        start_date = xml.at_xpath("ns0:startDate/text()", VBMS::XML_NAMESPACES)
-        submit_date = xml.at_xpath("ns0:submitDate/text()", VBMS::XML_NAMESPACES)
+        data = XMLHelper.convert_to_hash(xml.to_xml)[:list_of_contentions]
 
         new(
-          id: xml["id"],
-          text: xml["title"],
-          start_date: start_date && Time.parse(start_date.content).to_date,
-          submit_date: submit_date && Time.parse(submit_date.content).to_date
+          id: data[:@id],
+          text: data[:@title],
+          start_date: data[:start_date],
+          submit_date: data[:submit_date]
         )
       end
     end
