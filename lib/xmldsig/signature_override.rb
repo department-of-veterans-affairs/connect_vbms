2f75a84143d7adb78a5752b855ea31d2342292b5
Nemo Djuric <nemo.djuric@gmail.com>
2018-05-11 10:58:53 -0400

    Rubocop

--- lib/xmldsig/signature_override.rb
+++ lib/xmldsig/signature_override.rb
@@ -36,7 +36,7 @@ module Xmldsig
         []
       end
     rescue NoMethodError
-        raise SOAPError.new("No InclusiveNamespaces found in SOAP response")
+      raise SOAPError.new("No InclusiveNamespaces found in SOAP response")
     end
 
     def canonicalized_signed_info
