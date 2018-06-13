b2ca9c4113b2497ef081f9e740de5b178c1ac71e
Kara Louie <kara@truss.works>
2018-06-12 17:04:52 -0700

    First pass at adding associate rated issues vbms v5

---
+++ lib/vbms/requests/associate_rated_issues.rb
@@ -0,0 +1,52 @@
+module VBMS
+  module Requests
+    class AssociateRatedIssues < BaseRequest
+      NAMESPACES = {
+        "xmlns:cla" => "http://vbms.vba.va.gov/external/ClaimService/v5",
+        "xmlns:cdm" => "http://vbms.vba.va.gov/cdm/claim/v5",
+        "xmlns:part" => "http://vbms.vba.va.gov/cdm/participant/v5"
+      }.freeze
+
+      # This all assumes that rated_issues is a hash in the form of:
+      # { issue_id: contention_id, issue_id2: contention_id2 }
+      def initialize(claim_id, rated_issues)
+        @claim_id = claim_id,
+        @rated_issues = rated_issues
+      end
+
+      def name
+        "associateRatedIssues"
+      end
+
+      def endpoint_url(base_url)
+        "#{base_url}#{VBMS::ENDPOINTS[:claims]}"
+      end
+
+      def soap_doc
+        VBMS::Requests.soap(more_namespaces: NAMESPACES) do |xml|
+          xml["cla"].associateRatedIssues do
+            @rated_issues.each do |rated_issue_id, contention_id|
+              xml["cla"].ratedIssueToAssociate(
+                rated_issue_id: rated_issue_id,
+                contention_id: contention_id,
+                claim_id: @claim_id
+              )
+            end
+          end
+        end
+      end
+
+      def signed_elements
+        [["/soapenv:Envelope/soapenv:Body",
+          { soapenv: SoapScum::XMLNamespaces::SOAPENV },
+          "Content"]]
+      end
+
+      def handle_response(doc)
+        # res = doc.at_xpath(
+        #   "//associateRatedIssuesResponse:ns2:wasRatedIssueAssociated")
+        puts doc
+      end
+    end
+  end
+end
