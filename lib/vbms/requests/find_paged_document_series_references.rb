ebadc30132f651ad8f15a090ffa55e11947d474e
Peter Karman <peter@truss.works>
2019-12-18 15:56:34 -0600

    rename internal variable

--- lib/vbms/requests/find_paged_document_series_references.rb
+++ lib/vbms/requests/find_paged_document_series_references.rb
@@ -49,8 +49,8 @@ module VBMS
           document_references = result[:document_series_references]
           paging = result[:paging_reference]
           documents = XMLHelper.versions_as_array(document_references).map do |doc_ref|
-            XMLHelper.versions_as_array(doc_ref[:versions]).map do |version|
-              construct_response(version)
+            XMLHelper.versions_as_array(doc_ref[:versions]).map do |versions|
+              construct_response(versions)
             end
           end
           { paging: paging, documents: documents.flatten }
@@ -59,23 +59,23 @@ module VBMS
 
       private
 
-      def construct_response(result)
-        alt_doc_types = XMLHelper.find_hash_by_key(result[:metadata], "altDocType")
-        restricted = XMLHelper.find_hash_by_key(result[:metadata], "restricted")
+      def construct_response(versions)
+        alt_doc_types = XMLHelper.find_hash_by_key(versions[:metadata], "altDocType")
+        restricted = XMLHelper.find_hash_by_key(versions[:metadata], "restricted")
         OpenStruct.new(
-          document_id: result[:@document_version_ref_id],
-          series_id: result[:@document_series_ref_id],
-          version: result[:version][:@major],
-          type_description: type_description(result),
-          type_id: type_id(result),
-          doc_type: type_id(result),
-          subject: result[:@subject],
-          received_at: result[:va_receive_date],
-          source: source(result),
-          mime_type: result[:@mime_type],
+          document_id: versions[:@document_version_ref_id],
+          series_id: versions[:@document_series_ref_id],
+          version: versions[:version][:@major],
+          type_description: type_description(versions),
+          type_id: type_id(versions),
+          doc_type: type_id(versions),
+          subject: versions[:@subject],
+          received_at: versions[:va_receive_date],
+          source: source(versions),
+          mime_type: versions[:@mime_type],
           alt_doc_types: alt_doc_types.present? ? JSON.parse(alt_doc_types[:value]) : nil,
           restricted: restricted.present? ? restricted[:value] : nil,
-          upload_date: result[:vbms_upload_date]
+          upload_date: versions[:vbms_upload_date]
         )
       end
 
