46f5006a2fcc7d09860f6702e6c167ae6f1d4a97
Sjones352 <sdjones@go-ct.com>
2020-02-05 18:37:58 -0500

    disabled Metrics/AbcSize

--- lib/vbms/requests/establish_claim.rb
+++ lib/vbms/requests/establish_claim.rb
@@ -38,6 +38,7 @@ module VBMS
 
       # More information on what the fields mean, see:
       # https://github.com/department-of-veterans-affairs/dsva-vbms/issues/66#issuecomment-266098034
+      # rubocop:disable Metrics/AbcSize
       def soap_doc
         # Gender should only be included if it is differentiated
         differentiated_gender = @veteran_record[:sex] ? { "gender" => @veteran_record[:sex] } : {}
@@ -94,6 +95,7 @@ module VBMS
           end
         end
       end
+      # rubocop:enable Metrics/AbcSize
 
       def signed_elements
         [["/soapenv:Envelope/soapenv:Body",
