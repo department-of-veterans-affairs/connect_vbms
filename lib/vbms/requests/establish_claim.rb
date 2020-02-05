f2238c844f11c6ca4f5d3fe75faac6cdefe89fc1
Sjones352 <sdjones@go-ct.com>
2020-02-05 17:44:01 -0500

    set status_type_code

--- lib/vbms/requests/establish_claim.rb
+++ lib/vbms/requests/establish_claim.rb
@@ -70,7 +70,7 @@ module VBMS
 
             xml["cla"].claimToEstablish({
               "benefitTypeCd" => @claim[:benefit_type_code], # C&P Live = '1', C&P Death = '2'
-              "claimLevelStatusCd" => @claim[:status_type_code].present? ? "RFD" : "PEND",
+              "claimLevelStatusCd" => @claim[:status_type_code].present? ? @claim[:status_type_code] : "PEND",
               "payeeCd" => @claim[:payee_code],
               "label" => @claim[:end_product_label],
               "modifiedEndProductCd" => @claim[:end_product_modifier],
