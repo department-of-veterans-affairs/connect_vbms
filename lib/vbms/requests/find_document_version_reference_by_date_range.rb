b431306984346bcf51781cb65bd9ee74fc5d5e51
Matt Ray <matthew.ray@va.gov>
2022-12-07 14:46:24 -0600

    Tests pass for document version by date range

--- lib/vbms/requests/find_document_version_reference_by_date_range.rb
+++ lib/vbms/requests/find_document_version_reference_by_date_range.rb
@@ -1,37 +1,34 @@
 # frozen_string_literal: true
 
 module VBMS
-    module Requests
-        # This call returns a list of document version references matching the veteran and date range
-        class FindDocumentVersionReferenceByDateRange < FindDocumentVersionReference
-            def initialize(file_number:, begin_date_range:, end_date_range:)
-                @file_number = file_number
-                @begin = begin_date_range
-                @end = end_date_range
-            end
+  module Requests
+    # This call returns a list of document version references matching the veteran and date range
+    class FindDocumentVersionReferenceByDateRange < FindDocumentVersionReference
+      def initialize(file_number:, begin_date_range:, end_date_range:)
+        super(file_number)
+        @begin = begin_date_range
+        @end = end_date_range
+      end
 
-            def name
-                "findDocumentVersionReferenceByDateRange"
-            end
+      def name
+        "findDocumentVersionReferenceByDateRange"
+      end
 
-            def soap_doc
-                VBMS::Requests.soap do |xml|
-                    xml["read"].findDocumentVersionReference do
-                        xml["read"].citeria do
-                            xml["v5"].veteran(
-                                "fileNumber" => @file_number
-                            )
-                            xml["v5"].vbmsUploadDate(
-                                "begin" => @begin,
-                                "end" => @end
-                            )
-                        end
-                    end                    
-                end  
+      def soap_doc
+        VBMS::Requests.soap do |xml|
+          xml["read"].findDocumentVersionReference do
+            xml["read"].citeria do
+              xml["v5"].veteran(
+                "fileNumber" => @file_number
+              )
+              xml["v5"].vbmsUploadDate(
+                "begin" => @begin,
+                "end" => @end
+              )
             end
+          end
         end
+      end
     end
+  end
 end
-
-
-
---
+++ spec/requests/find_document_version_reference_by_date_range_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+describe VBMS::Requests::FindDocumentVersionReferenceByDateRange do
+  describe "soap_doc" do
+    subject do
+      VBMS::Requests::FindDocumentVersionReferenceByDateRange.new(
+        file_number: "784449089", begin_date_range: Time.new(2022, 10, 10), end_date_range: Time.now
+      )
+    end
+    it "generates valid SOAP" do
+      xml = subject.soap_doc.to_xml
+      xsd = Nokogiri::XML::Schema(fixture("soap.xsd"))
+      expect(xsd.errors).to eq []
+      errors = xsd.validate(parse_strict(xml))
+      expect(errors).to eq []
+    end
+  end
+  describe "parsing the XML response" do
+    before(:all) do
+      request = VBMS::Requests::FindDocumentVersionReferenceByDateRange.new(
+        file_number: "784449089", begin_date_range: Time.new(2022, 10, 10), end_date_range: Time.now
+      )
+      xml = fixture("responses/find_document_version_reference_response.xml")
+      doc = parse_strict(xml)
+      @vbms_docs = request.handle_response(doc)
+    end
+    subject { @vbms_docs }
+    it "should return an array of documents" do
+      expect(subject).to be_an(Array)
+      expect(subject.count).to be 124 # how many are in the sample file
+    end
+    it "should load contents correctly" do
+      doc1 = subject.first
+      expect(doc1[:document_id]).to eq "{CB958142-F063-489D-80DC-6C8A7A5B4319}"
+      expect(doc1[:series_id]).to eq "{623D6B8A-D599-4E2D-947E-A2CA2955C4DD}"
+      expect(doc1[:version]).to eq "2"
+      expect(doc1[:type_description]).to eq "C&#38;P Exam"
+      expect(doc1[:type_id]).to eq "356"
+      expect(doc1[:source]).to eq "VHA_CUI"
+      expect(doc1[:subject]).to eq "knee"
+      expect(doc1[:restricted]).to eq false
+      expect(doc1[:received_at]).to eq Date.parse("2017-03-29-04:00")
+      expect(doc1[:upload_date]).to eq Date.parse("2017-03-29-04:00")
+    end
+  end
+end
